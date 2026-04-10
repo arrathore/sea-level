0f7dace82ebba2350993d27bb9a528fde2e5e0f9
Apoorav Rathore <apoorav.rathore@icloud.com>
2026-04-10 14:05:19 -0500

    started computing gmsl from gridded vals

--- gridded_analysis.py
+++ gridded_analysis.py
@@ -2,6 +2,7 @@ import xarray as xr
 import pandas as pd
 import glob
 import re
+import numpy as np
 
 # load dataset from all files
 files = sorted(glob.glob("datasets/NASA_SSH_REF_SIMPLE_GRID_V1/NASA-SSH_alt_ref_simple_grid_v1_*.nc"))
@@ -18,8 +19,24 @@ for f in files:
     datasets.append(ds)
 
 # print info
-ds = xr.concat(datasets, dim="time")
+ds = xr.concat(datasets, dim="time").chunk({"time": 10})
 print(ds)
 
+# convert longitude from 0, 360 to -180, 180
+ds = ds.rename({"latitude": "lat", "longitude": "lon"})
+ds = ds.assign_coords(lon=(((ds.lon + 180) % 360) - 180))
+ds = ds.sortby("lon")
+
+# handle missing values
+sla = ds.ssha.where(ds.ssha < 1e10)
+
+# compute global mean sea level
+weights = np.cos(np.deg2rad(ds.lat))
+weights.name = "weights"
+
+gmsl = sla.weighted(weights).mean(dim=["lat", "lon"])
+gmsl = gmsl * 1000 # convert to mm
+gmsl = gmsl.compute()
+print('gmsl:', gmsl[:10])
 
 
